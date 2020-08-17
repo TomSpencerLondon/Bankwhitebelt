d99d588da3637a386473b7e384c535458ca37f25
Tom <thomas.spencer@codurance.com>
2020-08-17 07:21:39 +0100

    Add tests

--- Bank.Test/Acceptance/PrintStatementFeature.cs
+++ Bank.Test/Acceptance/PrintStatementFeature.cs
@@ -16,8 +16,8 @@ namespace Bank.Test.Acceptance
 
             clockMoq.SetupSequence(clock => clock.DateAsString())
                 .Returns("10/04/2014")
-                .Returns("2/04/2014")
-                .Returns("1/04/2014");
+                .Returns("02/04/2014")
+                .Returns("01/04/2014");
             
             var account = new Account(clockMoq.Object, consoleMoq.Object);
 
